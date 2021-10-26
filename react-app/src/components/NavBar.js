ad2159f9387d51b53dd5dd3ba02cfb872f71bbe9
Blair Wishom <blwishom@gmail.com>
2021-10-26 11:20:29 -0700

    more styling

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -8,49 +8,47 @@ const NavBar = () => {
   return (
     <nav className="navbar">
       <ul className="navbar-ul">
-        <li>
           <NavLink to='/' exact={true} activeClassName='active'>
             Home
           </NavLink>
-        </li>
-        <li>
+
           <NavLink to='/notes' exact={true} activeClassName='active'>
             Notes
           </NavLink>
-        </li>
-        <li>
+
+    
           <NavLink to='/notes/new' exact={true} activeClassName='active'>
             Create Notes
           </NavLink>
-        </li>
-        <li>
+      
+    
           <NavLink to='/notebooks' exact={true} activeClassName='active'>
             Notebooks
           </NavLink>
-        </li>
-        <li>
+      
+    
           <NavLink to='/notebooks/new' exact={true} activeClassName='active'>
             Create Notebooks
           </NavLink>
-        </li>
-        <li>
+      
+    
           <NavLink to='/login' exact={true} activeClassName='active'>
             Login
           </NavLink>
-        </li>
-        <li>
+      
+    
           <NavLink to='/sign-up' exact={true} activeClassName='active'>
             Sign Up
           </NavLink>
-        </li>
-        <li>
+      
+    
           <NavLink to='/users' exact={true} activeClassName='active'>
             Users
           </NavLink>
-        </li>
-        <li>
+      
+    
           <LogoutButton />
-        </li>
+      
       </ul>
     </nav>
   );
