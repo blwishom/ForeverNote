1f243c8d49d0a785bb996ceb242e46eafe9d1cb2
Blair Wishom <blwishom@gmail.com>
2022-03-30 15:12:23 -0700

    added delete modal to search page

--- react-app/src/components/SearchNotePage/index.js
+++ react-app/src/components/SearchNotePage/index.js
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router";
 import EditForm from "../EditForm";
+import DeleteModal from "./delete_modal";
 import { FaRegEdit, FaTrashAlt } from "react-icons/fa";
 import { Redirect } from "react-router";
 import { useSelector } from "react-redux";
 import './index.css';
 
-const SearchNotePage = () => {
+const SearchNotePage = ({ closeModal }) => {
     const [note, setNote] = useState('');
     const {noteId} = useParams();
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
+    const [openDeleteModal, setOpenDeleteModal] = useState(false);
+    const [deletedNoteId, setDeletedNoteId] = useState(-1);
+    const [notes, setNotes] = useState([]);
+    const [noteId2, setNoteId2] = useState(-1);
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
     const [editing, setEditing] = useState(false);
     const [editedTitle, setEditedTitle] = useState(false);
     const [editedContent, setEditedContent] = useState(false);
+    const [noteCreated, setNoteCreated] = useState(false);
     const [noteDeleted, setNoteDeleted] = useState(false);
     const user = useSelector((state) => state.session.user);
     const history = useHistory();
@@ -33,23 +39,40 @@ useEffect(() => {
 
 
 // Delete note
-async function deleteNote(noteId) {
-    const res = await fetch(`/api/notes/${noteId}`, {
-        method: "DELETE",
-    });
-    if (res.ok) {
-        const note = await res.json();
-            setNoteDeleted(!noteDeleted);
-            history.push('/notes');
-    } else {
-        return "No note has been retrieved"
-    }
-    return res;
+// async function deleteNote(noteId) {
+//     const res = await fetch(`/api/notes/${noteId}`, {
+//         method: "DELETE",
+//     });
+//     if (res.ok) {
+//         const note = await res.json();
+//             setNoteDeleted(!noteDeleted);
+//             history.push('/notes');
+//     } else {
+//         return "No note has been retrieved"
+//     }
+//     return res;
+// }
+
+//     if (!user) {
+//         return <Redirect to='/login' />;
+//         };
+
+function modalFunction(noteId) {
+    setDeletedNoteId(noteId);
+    setOpenDeleteModal(true);
 }
 
-    if (!user) {
-        return <Redirect to='/login' />;
-        };
+function editing_Note(noteNumber, noteTitle, noteContent) {
+    setEditing(!editing);
+    setTitle(noteTitle);
+    setContent(noteContent);
+    setNoteId2(noteNumber);
+}
+
+
+if (!user) {
+    return <Redirect to='/login' />;
+  }
 
     return (
         <>
@@ -64,7 +87,8 @@ async function deleteNote(noteId) {
                 {note?.content}
                 <div className='search-edit-delete-div'>
                     <button className="note-page-edit-btn" onClick={() => setEditing(!editing)}><FaRegEdit /></button>
-                    <button className="note-page-delete-btn" onClick={() => deleteNote(note.id)}><FaTrashAlt /></button>
+                    <button className="note-page-delete-btn" onClick={() => modalFunction(note.id)}><FaTrashAlt /></button>
+                    {(openDeleteModal && deletedNoteId===note.id) && <DeleteModal setOpenDeleteModal={setOpenDeleteModal} noteId={note.id} />}
                 </div>
             </div>
         </div>}
